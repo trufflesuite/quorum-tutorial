5a6ba7912c123d09dab88500f146a37e69e216ff
CruzMolina <7537712+CruzMolina@users.noreply.github.com>
2019-06-11 14:25:54 -0700

    quorum tutorial solution

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,19 @@
+// File: `./contracts/SimpleStorage.sol`
+
+pragma solidity >=0.4.21 <0.6.0;
+
+contract SimpleStorage {
+  uint public storedData;
+
+  constructor(uint initVal) public {
+    storedData = initVal;
+  }
+
+  function set(uint x) public {
+    storedData = x;
+  }
+
+  function get() view public returns (uint retVal) {
+    return storedData;
+  }
+}
---
+++ truffle-config.js
@@ -0,0 +1,29 @@
+// File: `truffle-config.js`
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 22000, // was 8545
+      network_id: "*", // Match any network id
+      gasPrice: 0,
+      gas: 4500000,
+      type: "quorum" // needed for Truffle to support Quorum
+    },
+    nodefour:  {
+      host: "127.0.0.1",
+      port: 22003,
+      network_id: "*", // Match any network id
+      gasPrice: 0,
+      gas: 4500000,
+      type: "quorum" // needed for Truffle to support Quorum
+    },
+    nodeseven:  {
+      host: "127.0.0.1",
+      port: 22006,
+      network_id: "*", // Match any network id
+      gasPrice: 0,
+      gas: 4500000,
+      type: "quorum" // needed for Truffle to support Quorum
+    }
+  }
+};
